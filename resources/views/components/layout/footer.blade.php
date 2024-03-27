120c062a35ad51c86589d6742fe3cbd2a26250e2
Marcel Stadelmann <m@marceli.to>
2024-03-27 13:32:31 +0100

    wip

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -57,15 +57,6 @@
 </footer>
 @livewireScripts
 @vite('resources/js/app.js')
-{{-- 
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-RWPD2SW544"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-RWPD2SW544');
-</script>
---}}
 </body>
 </html>
 <!-- made with ❤ by stoz.ch & marceli.to -->
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -19,6 +19,13 @@
       <h1>Wohnen im schönen Zürcher Oberland</h1>
       <p>An der <a href="https://maps.app.goo.gl/96gni24YkQ97FwuQA" target="_blank" title="Auf Googlemaps anzeigen" class="underline hover:no-underline underline-offset-4 decoration-1">Pfaffbergstrasse 2 und 4 in Pfäffikon ZH</a> werden ab dem Sommer 2024 zwei moderne Mehrfamilienhäuser, in grüner Umgebung, mit je acht Eigentumswohnungen realisiert – drei im Erdgeschoss, drei im Obergeschoss und zwei im Attikageschoss. Es entstehen 3.5- und 4.5-Zimmerwohnungen, ideal für Paare an ruhiger Lage.</p>
       <p>Haben wir Ihr Interesse geweckt? Gerne senden wir Ihnen weitere Informationen, sobald die Vermarktung startet. Bitte füllen Sie dazu das Kontaktformular aus.</p>
+      <p>
+        <a href="/media/downloads/wohnen-felsengrund-verkaufsplaene.pdf" 
+          target="_blank" 
+          class="bg-caramel rounded-sm font-semi font-semibold inline-flex text-white uppercase py-10 px-20 tracking-wider leading-none hover:bg-charcoal transition-colors">
+          Verkaufspläne
+        </a>
+      </p>
     </div>
   </x-layout.inner>
 </x-layout.section>
