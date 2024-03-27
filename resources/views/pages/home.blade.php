cf85eb0b4f37cef6198e5440678bce99bed0820f
Marcel Stadelmann <m@marceli.to>
2024-03-27 18:52:41 +0100

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -19,13 +19,32 @@
       <h1>Wohnen im schönen Zürcher Oberland</h1>
       <p>An der <a href="https://maps.app.goo.gl/96gni24YkQ97FwuQA" target="_blank" title="Auf Googlemaps anzeigen" class="underline hover:no-underline underline-offset-4 decoration-1">Pfaffbergstrasse 2 und 4 in Pfäffikon ZH</a> werden ab dem Sommer 2024 zwei moderne Mehrfamilienhäuser, in grüner Umgebung, mit je acht Eigentumswohnungen realisiert – drei im Erdgeschoss, drei im Obergeschoss und zwei im Attikageschoss. Es entstehen 3.5- und 4.5-Zimmerwohnungen, ideal für Paare an ruhiger Lage.</p>
       <p>Haben wir Ihr Interesse geweckt? Gerne senden wir Ihnen weitere Informationen, sobald die Vermarktung startet. Bitte füllen Sie dazu das Kontaktformular aus.</p>
-      <p>
-        <a href="/media/downloads/wohnen-felsengrund-verkaufsplaene.pdf" 
-          target="_blank" 
-          class="bg-caramel rounded-sm font-semi font-semibold inline-flex text-white uppercase py-10 px-20 tracking-wider leading-none hover:bg-charcoal transition-colors">
-          Verkaufspläne
-        </a>
-      </p>
+      <div class="mt-15 md:mt-25 md:flex space-y-10 md:space-y-0 md:space-x-15">
+        <div>
+          <a href="/media/downloads/wohnen-felsengrund-verkaufsplaene.pdf" 
+            target="_blank" 
+            title="Verkaufspläne herunterladen"
+            class="bg-caramel rounded-sm font-semi font-semibold inline-flex text-white uppercase py-10 px-20 tracking-wider leading-none hover:bg-charcoal transition-colors">
+            Verkaufspläne
+          </a>
+        </div>
+        <div>
+          <a href="/media/downloads/wohnen-felsengrund-untergeschoss-und-tiefgaragenplan.pdf" 
+            target="_blank" 
+            title="Untergeschoss / Tiefgaragenplan herunterladen"
+            class="bg-caramel rounded-sm font-semi font-semibold inline-flex text-white uppercase py-10 px-20 tracking-wider leading-none hover:bg-charcoal transition-colors">
+            UG- und TG-Pläne
+          </a>
+        </div>
+        <div>
+          <a href="/media/downloads/wohnen-felsengrund-umgebungsplan.pdf" 
+            target="_blank" 
+            title="Umgebungsplan herunterladen"
+            class="bg-caramel rounded-sm font-semi font-semibold inline-flex text-white uppercase py-10 px-20 tracking-wider leading-none hover:bg-charcoal transition-colors">
+            Umgebungsplan
+          </a>
+        </div>
+      </div>
     </div>
   </x-layout.inner>
 </x-layout.section>
